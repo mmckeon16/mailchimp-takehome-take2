1842e2f3a5542d7cf48507ee717fddde51519bab
mmckeon16 <mmckeon204@gmail.com>
2022-04-19 21:21:16 -0400

    feat: adding empty and p tags

--- index.js
+++ index.js
@@ -6,10 +6,12 @@
 
 const fs = require("fs");
 
-const regexH1 = /#\s[\s\S]*/;
+const regexH1 = /^#\s[\s\S]*/;
 const regexH1Content = /(?<=#\s)[\s\S]*/;
-const regexH2 = /##\s[\s\S]*/;
+const regexH2 = /^##\s[\s\S]*/;
 const regexH2Content = /(?<=##\s)[\s\S]*/;
+const regexP = /.*/;
+const regexEmpty = /./;
 
 fs.readFile("./test.md", "utf8", (err, data) => {
   if (err) {
@@ -17,36 +19,34 @@ fs.readFile("./test.md", "utf8", (err, data) => {
     return;
   }
   const splitDataArray = data.split("\n");
+  let html = "";
 
   for (let i = 0; i < splitDataArray.length; i++) {
     if (splitDataArray[i].match(regexH2)) {
       const h2Content = splitDataArray[i].match(regexH2Content);
-      fs.writeFile(
-        "./index.html",
-        `<h2>${h2Content}</h2>\n`,
-        { flag: "a+" },
-        err => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          //file written successfully
-        }
-      );
+      html = html + `<h2>${h2Content}</h2>\n`;
     } else if (splitDataArray[i].match(regexH1)) {
       const h1Content = splitDataArray[i].match(regexH1Content);
-      fs.writeFile(
-        "./index.html",
-        `<h1>${h1Content}</h1>\n`,
-        { flag: "a+" },
-        err => {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          //file written successfully
-        }
-      );
+      html = html + `<h1>${h1Content}</h1>\n`;
+    } else if (splitDataArray[i].match(regexEmpty) === null) {
+      continue;
+    } else if (splitDataArray[i].match(regexP)) {
+      html = html + `<p>${splitDataArray[i]}</p>\n`;
     }
   }
+
+  // write once to index.html
+  fs.writeFile(
+    "./index.html",
+    html,
+    // flag to add to end of file
+    // { flag: "a+" },
+    err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      //file written successfully
+    }
+  );
 });
--- test.md
+++ test.md
@@ -1,7 +1,5 @@
 # Implement a Markdown => HTML converter
 
-Markdown is a simple syntax used to generate formatted text. It’s used in lots
-of places, but the one most developers have probably encountered is README
-files in github.
+Markdown is a simple syntax used to generate formatted text. It’s used in lots of places, but the one most developers have probably encountered is README files in github.
 
 ## Formatting Specifics
